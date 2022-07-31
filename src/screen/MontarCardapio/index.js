db500cfdd6939fa4186c20e77494c6dab6610bde
JALVIVI <jalvivi@pop-os.localdomain>
2022-07-31 20:53:41 -0300

    Tela monta cardápio

--- src/screen/MontarCardapio/index.js
+++ src/screen/MontarCardapio/index.js
@@ -1,37 +1,45 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import React, { useState, useEffect, useContext } from 'react';
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React, {useState, useEffect, useContext} from 'react';
 import Estilo from '../../Style/Estilo';
-import { Card, Btn2 } from '../../components';
-import firestore from "@react-native-firebase/firestore";
-import { Picker } from '@react-native-picker/picker';
-import { CTX_SelecaoPrato } from '../../contexts';
+import {Card, Btn2} from '../../components';
+import firestore from '@react-native-firebase/firestore';
+import {Picker} from '@react-native-picker/picker';
+import {CTX_SelecaoPrato} from '../../contexts';
 import INF from '../../config/';
+import {MoedaReal} from '../../functions/mascaras';
 
 const pathDb = firestore().collection('Restaurante').doc(INF().ID_APP);
 
-export default function MontarCardapio({ navigation }) {
-
+export default function MontarCardapio({navigation}) {
   const [ctxSelecaoPrato, setCtxSelecaoPrato] = useContext(CTX_SelecaoPrato);
   const [prato, setPratos] = useState([]);
   const [valores, setValores] = useState([]);
   const [medidas, setMedidas] = useState([]);
+  const [acompanhamento, setAcampanhamento] = useState([]);
+  const [arrayValores, setArrayValores] = useState([]);
   const [selecaoPickerPrato, setSelecaoPickerPrato] = useState('');
+  const [selecaoPickerAcompanhamento, setSelecaoPickerAcompanhamento] = useState('');
   const [atualiza, setAtualiza] = useState(0);
 
   useEffect(() => {
     const listaPratos = async () => {
       const l = await pathDb.collection('Pratos').orderBy('nome_prato').get();
       setPratos(l.docs);
-    }
+    };
     const listaValores = async () => {
       const l = await pathDb.collection('Valores').orderBy('valor').get();
       setValores(l.docs);
-    }
+    };
     const listaMedidas = async () => {
       const l = await pathDb.collection('Medidas').orderBy('medida').get();
       setMedidas(l.docs);
-    }
+    };
+    const listaAcompanhamentos = async () => {
+      const l = await pathDb.collection('Acompanhamento').orderBy('acompanhamento').get();
+      setAcampanhamento(l.docs);
+    };
 
+    listaAcompanhamentos();
     listaMedidas();
     listaValores();
     listaPratos();
@@ -39,110 +47,154 @@ export default function MontarCardapio({ navigation }) {
   }, [atualiza]);
 
   const ValorPickerPratos = () => {
-    const l = prato.map((prato, index) => {
-      return <Picker.Item key={index} label={prato.data().nome_prato} value={prato.data().nome_prato} />;
-    })
-
-    return l;
-  }
-
-  const ValorPickerValores = () => {
-    const l = valores.map((valores, index) => {
-      return <Picker.Item key={index} label={valores.data().valor} value={valores.data().valor} />;
-    })
-
-    return l;
-  }
-
-  const ValorPickerMedidas = () => {
-    const l = medidas.map((medidas, index) => {
-      return <Picker.Item key={index} label={medidas.data().medida} value={medidas.data().medida} />;
-    })
-
-    return l;
-  }
-
-
-  const PickerTamanhoValor = () => {
-
-    const lTamanho = medidas.map((med, index)=> {
-      
+    const lp = prato.map((prato, index) => {
+      return (
+        <Picker.Item
+          key={index}
+          label={prato.data().nome_prato}
+          value={prato.data().nome_prato}
+        />
+      );
     });
 
-    return(
-      <View style={Estilo.boxNeutro}>
-                <View style={Estilo.BoxRow}>
-                  {/* Coluna 1 */}
-                  <View style={Estilo.boxColunaSelecao1}>
-                    <Text style={Estilo.TxtComum}>Grande</Text>
-                  </View>
-                  {/* Coluna 2 */}
-                  <View style={Estilo.boxColunaSelecao2}>
-                    <Picker dropdownIconColor='#2D2D3F' style={{ backgroundColor: '#fff' }} selectedValue={prato} onValueChange={prato => { setPrato(prato) }}>
-                      <Picker.Item label='Nenhum' value='' />
-                      <Picker.Item label='R$14,56' value='14.56' />
-                    </Picker>
-                  </View>
-                </View>
-              </View>
-    );
-  }
+    return lp;
+  };
 
+  const ValorPickerAcompanhamento = () => {
+    const lp = acompanhamento.map((acmp, index) => {
+      return (
+        <Picker.Item
+          key={index}
+          label={acmp.data().acompanhamento}
+          value={acmp.data().acompanhamento}
+        />
+      );
+    });
 
+    return lp;
+  };
+
+  const ValorPickerValores = () => {
+    const lv =valores.map((valores, index) => {
+      return (
+        <Picker.Item
+          key={index}
+          label={MoedaReal(valores.data().valor)}
+          value={valores.data().valor}
+        />
+      );
+    });
+
+    return lv;
+  };
+
+  const PickerTamanhoValor = () => {
+    const LTamanho = medidas.map((med, index) => {
+      return (
+        <View key={index} style={Estilo.boxNeutro}>
+          <View style={Estilo.BoxRow}>
+            <View style={Estilo.boxColunaSelecao1}>
+              <Text style={Estilo.TxtComum}>{med.data().medida}</Text>
+            </View>
+
+            <View style={Estilo.boxColunaSelecao2}>
+              <Picker
+                mode="dropdown"
+                dropdownIconColor="#2D2D3F"
+                style={{backgroundColor: '#fff'}}
+                onValueChange={selecaoPickerValor => {
+                  arrayValores[index] = {
+                    medida: med.data().medida,
+                    valor: selecaoPickerValor,
+                    indice: index,
+                  };
+
+                  setArrayValores(arrayValores);
+                  //console.log(arrayValores);
+                  setAtualiza(1);
+                }}
+                selectedValue={
+                  arrayValores[index] != undefined
+                    ? arrayValores[index].valor
+                    : 'Selecione o valor'
+                }>
+                <Picker.Item label="Selecione o valor" value="" />
+                {ValorPickerValores()}
+                <Picker.Item label="Desativar" value={null} />
+              </Picker>
+            </View>
+          </View>
+        </View>
+      );
+    });
+
+    return LTamanho;
+  };
 
   return (
     <SafeAreaView>
-      <Card titulo="Montar Cardápio">
-        <View>
+      <ScrollView>
+        <Card titulo="Montar Cardápio">
           <View>
-            {/*===================================================
-            == Seleção de Prato
-            ====================================================*/}
-
             <View>
-              <View style={Estilo.boxNeutro}>
-                <Text style={Estilo.TxtComum}>Selecione o prato</Text>
-              </View>
-              <Picker
-                dropdownIconColor='#2D2D3F'
-                style={{ backgroundColor: '#fff' }}
-                selectedValue={selecaoPickerPrato}
-                onValueChange={selecaoPickerPrato => {
-                  setSelecaoPickerPrato(selecaoPickerPrato);
-                }}>
-                <Picker.Item label='Nenhum' value='' />
-                {ValorPickerPratos()}
-              </Picker>
-            </View>
-
-            {/*===================================================
-            == Seleção de Valores e tamanho
-            ====================================================*/}
-            <View style={Estilo.Dividir} />
-            <View>
-              <View style={Estilo.BoxRow}>
-                {/* Coluna 1 */}
-                <View style={Estilo.boxColunaSelecao1}>
-                  <Text style={Estilo.H2}>Tamanho:</Text>
+              <View>
+                <View style={Estilo.boxNeutro}>
+                  <Text style={Estilo.TxtComum}>Selecione o prato</Text>
                 </View>
-                {/* Coluna 2 */}
-                <View style={Estilo.boxColunaSelecao2}>
-                  <Text style={Estilo.H2}>Valor:</Text>
-                </View>
+                <Picker
+                  mode="dropdown"
+                  dropdownIconColor="#2D2D3F"
+                  style={{backgroundColor: '#fff'}}
+                  selectedValue={selecaoPickerAcompanhamento}
+                  onValueChange={selecaoPickerAcompanhamento => {
+                    setSelecaoPickerAcompanhamento(selecaoPickerAcompanhamento);
+                  }}>
+                  <Picker.Item label="Nenhum" value="" />
+                  {ValorPickerPratos()}
+                </Picker>
               </View>
 
-              {/* Aqui entra o campo de tamanho e valor */}
-              {PickerTamanhoValor()}
+              <View>
+                <View style={Estilo.boxNeutro}>
+                  <Text style={Estilo.TxtComum}>Selecione o acompanhamento</Text>
+                </View>
+                <Picker
+                  mode="dropdown"
+                  dropdownIconColor="#2D2D3F"
+                  style={{backgroundColor: '#fff'}}
+                  selectedValue={selecaoPickerPrato}
+                  onValueChange={selecaoPickerPrato => {
+                    setSelecaoPickerPrato(selecaoPickerPrato);
+                  }}>
+                  <Picker.Item label="Nenhum" value="" />
+                  {ValorPickerAcompanhamento()}
+                </Picker>
+              </View>
 
+              <View style={Estilo.Dividir} />
+              <View>
+                <View style={Estilo.BoxRow}>
+                  <View style={Estilo.boxColunaSelecao1}>
+                    <Text style={Estilo.H2}>Tamanho:</Text>
+                  </View>
+                  <View style={Estilo.boxColunaSelecao2}>
+                    <Text style={Estilo.H2}>Valor:</Text>
+                  </View>
+                </View>
+                {PickerTamanhoValor()}
+              </View>
+            </View>
+            <View style={Estilo.boxNeutro}>
+              <Btn2
+                txt="Salvar"
+                fncClique={() => {
+                  navigation.navigate('Cadastro');
+                }}
+              />
             </View>
           </View>
-          <View style={Estilo.boxNeutro}>
-            <Btn2 txt="Salvar" fncClique={() => {
-              navigation.navigate("Cadastro");
-            }} />
-          </View>
-        </View>
-      </Card>
+        </Card>
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
