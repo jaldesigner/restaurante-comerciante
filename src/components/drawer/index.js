6b5b69776628be235cb4a0dce2960c582c3d0cb2
deartis <jalinittrader@gmail.com>
2022-08-13 23:42:11 -0300

    drawer

--- src/components/drawer/index.js
+++ src/components/drawer/index.js
@@ -1,18 +1,89 @@
-import { View, Text, } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import Estilo from '../../Style/Estilo';
+import { DrawerContent } from '@react-navigation/drawer';
 
 export default function Dwr(props) {
   return (
-    <View style={Estilo.ContainerGeral}>
-      <View style={Estilo.ItemCenter}>
-        <Text style={Estilo.H2}>ADM Painel</Text>
+    <View style={stl.container}>
+      <View style={stl.topo}>
+        <Text style={stl.titulo}>ADM Painel</Text>
       </View>
-      <View style={Estilo.Dividir} />
-      <View>
-      
+      <View style={stl.content}>
+        <View style={stl.boxLink}>
+          <TouchableOpacity style={stl.link}>
+            <Text style={stl.txtLink}>Home</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={stl.boxLink}>
+          <TouchableOpacity style={stl.link}>
+            <Text style={stl.txtLink}>Cardápio</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={stl.boxLink}>
+          <TouchableOpacity style={stl.link}>
+            <Text style={stl.txtLink}>Ordens</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={stl.boxLink}>
+          <TouchableOpacity style={stl.link}>
+            <Text style={stl.txtLink}>Cadastro</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={stl.boxLink}>
+          <TouchableOpacity style={stl.link}>
+            <Text style={stl.txtLink}>Administrar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={stl.boxLink}>
+          <TouchableOpacity style={stl.link}>
+            <Text style={stl.txtLinkSair}>Sair</Text>
+          </TouchableOpacity>
+        </View>
+        
       </View>
     </View>
   )
 }
+
+const stl = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2D2D3F',
+  },
+  content: {
+    padding: 10
+  },
+  topo: {
+    alignItems: 'center',
+    borderBottomColor: "#6C6D80",
+    borderBottomWidth: 1,
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  titulo: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  boxLink: { 
+    marginBottom: 10,
+    borderBottomColor: "#4E5063",
+    borderBottomWidth: 1,
+  },
+  link: {
+    alignItems: 'stretch',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  txtLink: {
+    color: '#61C6FF',
+    fontSize:18,
+  },
+  txtLinkSair: {
+    color: '#CF565F',
+    fontSize:18,
+  }
+
+
+});
--- src/router/Router.js
+++ src/router/Router.js
@@ -12,6 +12,7 @@ import CadastroPrato from '../screen/Cadastro/CadastroPrato';
 import CadastroAcompanhamento from '../screen/Cadastro/CadastroAcompanhamento';
 import CadastroValores from '../screen/Cadastro/CadastroValores';
 import CadastroMedidas from '../screen/Cadastro/CadastroMedidas';
+import { View } from 'react-native';
 
 //const RouterStack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -19,18 +20,18 @@ const Drawer = createDrawerNavigator();
 export default function AppRouter() {
 
   return (
+      <Drawer.Navigator initialRouteName="MontarCardapio" drawerContent={props => <Dwr {...props} />} >
+        <Drawer.Screen name='Home' component={Home} options={{ headerTransparent: false, headerShown: false }} />
+        <Drawer.Screen name='MontarCardapio' component={MontarCardapio} options={{ headerTransparent: true, headerShown: false }} />
 
-    <Drawer.Navigator initialRouteName="MontarCardapio" drawerContent={props => <Dwr {...props} />} >
-      <Drawer.Screen name='Home' component={Home} options={{ headerTransparent: false, headerShown: false }} />
-      <Drawer.Screen name='MontarCardapio' component={MontarCardapio} options={{ headerTransparent: true, headerShown: false }} />
-
-      {/** Cadastros de produtos */}
-      <Drawer.Screen name='Cadastro' component={Cadastro} options={{ headerTransparent: true, headerShown: false }} />
-      <Drawer.Screen name='CadastroPrato' component={CadastroPrato} options={{ headerTransparent: true, headerShown: false }} />
-      <Drawer.Screen name='CadastroAcompanhamento' component={CadastroAcompanhamento} options={{ headerTransparent: true, headerShown: false }} />
-      <Drawer.Screen name='CadastroValores' component={CadastroValores} options={{ headerTransparent: true, headerShown: false }} />
-      <Drawer.Screen name='CadastroMedidas' component={CadastroMedidas} options={{ headerTransparent: true, headerShown: false }} />
-    </Drawer.Navigator>
+        {/** Cadastros de produtos */}
+        <Drawer.Screen name='Cadastro' component={Cadastro} options={{ headerTransparent: true, headerShown: false }} />
+        <Drawer.Screen name='CadastroPrato' component={CadastroPrato} options={{ headerTransparent: true, headerShown: false }} />
+        <Drawer.Screen name='CadastroAcompanhamento' component={CadastroAcompanhamento} options={{ headerTransparent: true, headerShown: false }} />
+        <Drawer.Screen name='CadastroValores' component={CadastroValores} options={{ headerTransparent: true, headerShown: false }} />
+        <Drawer.Screen name='CadastroMedidas' component={CadastroMedidas} options={{ headerTransparent: true, headerShown: false }} />
+        
+      </Drawer.Navigator>
 
   );
 }
